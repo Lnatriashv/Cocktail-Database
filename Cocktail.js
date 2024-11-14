8fbadc3c7926f114bda7930112443306410a2072
emalak <melkhouli@constructor.university>
2024-11-14 13:56:31 +0100

    Assignment 9 : check index.html updated and Cocktail.jsgit

---
+++ Cocktail.js
@@ -0,0 +1,10 @@
+const cocktailsDB = [
+    { name: "Mojito", ingredients: ["White rum", "Sugar", "Lime juice", "Soda water", "Mint"] },
+    { name: "Margarita", ingredients: ["Tequila", "Triple sec", "Lime juice", "Salt"] },
+    { name: "Manhattan", ingredients: ["Whiskey", "Sweet vermouth", "Angostura bitters"] },
+    { name: "Mai Tai", ingredients: ["White rum", "Dark rum", "Orange curaçao", "Orgeat syrup"] },
+    { name: "Moscow Mule", ingredients: ["Vodka", "Ginger beer", "Lime juice"] },
+    { name: "Martini", ingredients: ["Gin", "Dry vermouth", "Olive"] },
+    { name: "Daiquiri", ingredients: ["White rum", "Lime juice", "Simple syrup"] },
+    { name: "Negroni", ingredients: ["Gin", "Campari", "Sweet vermouth"] }
+];
